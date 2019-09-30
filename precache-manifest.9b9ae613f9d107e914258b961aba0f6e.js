81bb9ae8389104818b87f13e903303303e23db16
BaptisteGarcin <baptistabomb86@gmail.com>
2019-09-30 22:28:45 +0200

    Updates

---
+++ precache-manifest.9b9ae613f9d107e914258b961aba0f6e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "51e7e1eba9b31869a16743300c29d412",
+    "url": "/covoit/index.html"
+  },
+  {
+    "revision": "9030e0db435f06ee8ece",
+    "url": "/covoit/static/css/main.f940b3ce.chunk.css"
+  },
+  {
+    "revision": "b67fbb6da8b775050473",
+    "url": "/covoit/static/js/2.7673cf29.chunk.js"
+  },
+  {
+    "revision": "9030e0db435f06ee8ece",
+    "url": "/covoit/static/js/main.df8e3147.chunk.js"
+  },
+  {
+    "revision": "2f7528439ad6a78082c6",
+    "url": "/covoit/static/js/runtime~main.e212bf09.js"
+  }
+]);

376d45687ddb2b7f2228f70a75b4ac04ccbb578e
BaptisteGarcin <baptistabomb86@gmail.com>
2019-10-03 23:24:20 +0200

    Updates

--- precache-manifest.6b59a59778a03bcea867e38ddce4b6a0.js
+++ precache-manifest.6b59a59778a03bcea867e38ddce4b6a0.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "92ddcbc69a065aee8b60a8a09a24e892",
+    "revision": "a88f1d19c61645c6b2e0f283ffbff5f1",
     "url": "/covoit/index.html"
   },
   {
-    "revision": "a46d66400b414aa7a5b7",
-    "url": "/covoit/static/css/main.60b57a4a.chunk.css"
+    "revision": "bc1182055f7920e06801",
+    "url": "/covoit/static/css/main.79e75ac8.chunk.css"
   },
   {
     "revision": "7fe3a39967007b858674",
     "url": "/covoit/static/js/2.331fd6bb.chunk.js"
   },
   {
-    "revision": "a46d66400b414aa7a5b7",
-    "url": "/covoit/static/js/main.6ba2da89.chunk.js"
+    "revision": "bc1182055f7920e06801",
+    "url": "/covoit/static/js/main.a7ee6c61.chunk.js"
   },
   {
     "revision": "2f7528439ad6a78082c6",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/covoit/precache-manifest.d56e72b1a6d7a260846a82ddf0918fff.js"
+  "/covoit/precache-manifest.6b59a59778a03bcea867e38ddce4b6a0.js"
 );
 
 self.addEventListener('message', (event) => {
